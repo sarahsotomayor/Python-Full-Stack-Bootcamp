50f30f0244a5b4553ffd12dbbc8f896fb8663d3a
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2022-11-29 20:06:38 -0700

    Add files via upload

---
+++ FUNDAMENTALS/string_practice.py
@@ -0,0 +1,18 @@
+print("This is a sample string")
+
+name = "Sarah"
+print("My name is", name)
+print("My name is " + name)
+
+Month = ("November ")
+Year = ("2022")
+print("The month and year are " + Month + Year)
+
+print("Practicing Commonly Used String Methods:")
+
+Hobbies = ("Gaming, reading, hiking, traveling, watching TV, collecting crystals")
+print(Hobbies.upper())
+print(Hobbies.lower())
+print(Hobbies.split())
+print(Hobbies.isalnum())
+print(Hobbies.isdigit())
