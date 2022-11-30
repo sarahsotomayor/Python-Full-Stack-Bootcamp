c710a98c783e0a2994e4d31ecf1112dc1c6aa1a7
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2022-11-29 19:24:06 -0700

    Add files via upload

---
+++ FUNDAMENTALS/FOR_LOOP_BASIC/for_loop_basic1.py
@@ -0,0 +1,40 @@
+# Print all integers from 0 to 150.
+for i in range(151):
+    print (i)
+
+# Print all the multiples of 5 from 5 to 1,000
+for i in range(5,1001,5):
+    print (i)
+
+# Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for i in range(101):
+    print (i)
+    if i%5 == 0:
+        print ("Coding")
+    if i%10 == 0:
+        print ("Coding Dojo")
+
+#Add odd integers from 0 to 500,000, and print the final sum.
+odd_total=0
+for number in range(1,500000):
+    if(number%2!=0):
+        odd_total=odd_total+number
+    if(number==5000000):
+        break
+print(odd_total)
+
+#Print positive numbers starting at 2018, counting down by fours.
+for i in range(2018,0,-4):
+    print(i)
+
+# Flexible Counter 
+"""
+Set three variables: lowNum, highNum, mult. Starting at lowNum and 
+going through highNum, print only the integers that are a multiple of mult. For example, if 
+lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+"""
+lowNum=1
+highNum=100
+multiple=10
+
+
