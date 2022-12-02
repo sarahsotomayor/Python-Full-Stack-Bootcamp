919dbf33bc8fecf887101c56f59218f0589eb0d2
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2022-12-01 18:03:16 -0700

    Update for_loop_basic1.py

--- FUNDAMENTALS/FOR_LOOP_BASIC/for_loop_basic1.py
+++ FUNDAMENTALS/FOR_LOOP_BASIC/for_loop_basic1.py
@@ -33,9 +33,9 @@ Set three variables: lowNum, highNum, mult. Starting at lowNum and
 going through highNum, print only the integers that are a multiple of mult. For example, if 
 lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
 """
-lowNum=1
-highNum=100
-multiple=10
+lowNum = 10
+highNum = 32
+mult = 5
 for i in range(lowNum, highNum):
     if i%mult==0:
         print(i)
